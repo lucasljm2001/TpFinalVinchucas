148f005de6aa8690d6f36eec7474c6d51cc89b25
Margo Cortizas <margarita.cortizas@gmail.com>
2023-05-13 20:15:55 -0300

    Sumo Test Usuario

--- test/aplicacion/vinchucas/muestra/UsuarioTest.java
+++ test/aplicacion/vinchucas/muestra/UsuarioTest.java
@@ -5,10 +5,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 import aplicacion.vinchucas.usuario.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
 import java.util.*;
 
 class UsuarioTest {
@@ -16,6 +19,7 @@ class UsuarioTest {
 	// SUT -> Usuario
 	Usuario lucas;
 	Usuario margo;
+	Usuario martin;
 	List<Muestra> muestras;
 	List<Usuario> usuarios;
 	Opinion opinion;
@@ -24,14 +28,18 @@ class UsuarioTest {
 	void setUp() {
 		margo = new Usuario("100");
 		lucas = new Usuario("200");
+		martin = new Usuario("300");
 		lucas.esExperto();
 		muestras = new ArrayList<Muestra>();
+				
 		for (int i=0; i < 40; i++) {
-			//usuarios.add(new Usuario("00" + i ));
 			muestras.add(mock(Muestra.class));		
+			when(muestras.get(i).getVerificacion()).thenReturn(mock(OBasico.class));
 		}
 		opinion = mock(Opinion.class);
-		
+		//deberiamos hacer que cada usuario opine una sola vez por cada muestra.
+		//y que no pueda opinar sobre una muestra que el mismo haya enviado.
+
 		
 	}
 	
@@ -39,10 +47,18 @@ class UsuarioTest {
 	@Test
 	void usuarioBasicoNoPuedeOpinarLuegoDelExperto() {
 		lucas.opinar(muestras.get(0), TipoDeOpinion.NINGUNA);
-		margo.opinar(muestras.get(0), TipoDeOpinion.CHINCHEFOLIADA);
 		verifyNoMoreInteractions(muestras.get(0));
+		margo.opinar(muestras.get(0), TipoDeOpinion.CHINCHEFOLIADA);
 		
 	}
 
-
+	@Test
+	void dosExpertosOpinanIgual() { 
+		margo.esExperto();
+		lucas.opinar(muestras.get(2), TipoDeOpinion.VINCHUCAGUASAYANA);
+		margo.opinar(muestras.get(2), TipoDeOpinion.VINCHUCAGUASAYANA);
+		verifyNoMoreInteractions(muestras.get(2));
+		martin.opinar(muestras.get(2), TipoDeOpinion.IMAGENPOCOCLARA);
+		
+	}
 }
